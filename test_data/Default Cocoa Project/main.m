0388ec028aaa6ae58e8c3333c0fd571ac20520a5
Aaron VonderHaar <gruen0aermel@gmail.com>
2009-03-28 23:36:21 -0700

    Added a basic Xcode project for testing.

---
+++ test_data/Default Cocoa Project/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Default Cocoa Project
+//
+//  Created by Aaron VonderHaar on 3/28/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
