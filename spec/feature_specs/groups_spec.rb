5724ac90cc11c3e8abcdfd5ef72aed47cafff8d2
Aaron VonderHaar <gruen0aermel@gmail.com>
2009-03-28 23:41:43 -0700

    New failing specs for listing groups.

---
+++ spec/feature_specs/groups_spec.rb
@@ -0,0 +1,20 @@
+require File.dirname(__FILE__) + '/../spec_helper.rb'
+
+describe "listing groups" do
+  
+  describe "Default Cocoa Project" do
+    before(:each) do
+      @project = File.dirname(__FILE__) + '/../../test_data/Default Cocoa Project/Default Cocoa Project.xcodeproj'
+      @reader = XcodeReader.new(@project)
+    end
+    
+    it "should return the number of groups" do
+      @reader.groups.size.should == 5
+    end
+    
+    it "should return the groups" do
+      @reader.groups.should == ["Classes", "Other Sources", "Resources", "Frameworks", "Products"]
+    end
+  end
+  
+end
